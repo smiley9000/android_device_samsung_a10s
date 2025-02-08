ba4021fba1621fcce640b203b9cdd0a2a94488f8
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-02-08 16:26:12 +0000

    a10s: build HIDL packages

--- device.mk
+++ device.mk
@@ -35,6 +35,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
